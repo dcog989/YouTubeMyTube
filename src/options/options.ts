215380195e57455d7d75b66a5f8da40a8aea429b
dcog989 <sirdavidlaws@gmail.com>
2026-09-19 14:39:03 +0100

    feat(options): show extension version in settings footer

--- src/options/options.ts
+++ src/options/options.ts
@@ -537,6 +537,7 @@ async function init(): Promise<void> {
   populate();
   setDirty(false);
   selectPanel('channels');
+  byId('app-version').textContent = `YouTubeMyTube v${chrome.runtime.getManifest().version}`;
   void backfillMissing();
 }
 
